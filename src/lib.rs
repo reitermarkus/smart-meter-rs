5aababfaf88edc9cc24ec8a137d289b5cc26d207
Jakob Leifhelm <jakob.leifhelm@gmail.com>
2022-07-11 11:32:28 +0200

    Update dlms_cosem

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,16 @@
 #![deny(missing_debug_implementations)]
 
-use std::fmt;
-use std::io::{self, Read};
-use std::num::NonZeroUsize;
+use std::{
+  fmt,
+  io::{self, Read},
+  marker::PhantomData,
+  num::NonZeroUsize,
+};
 
-use dlms_cosem::hdlc::HdlcDataLinkLayer;
-use dlms_cosem::{mbus::MBusDataLinkLayer, Apdu, Dlms, Error as DlmsError, ObisMap};
+use dlms_cosem::{
+  hdlc::HdlcDataLinkLayer, mbus::MBusDataLinkLayer, Apdu, Dlms, DlmsDataLinkLayer,
+  Error as DlmsError, ObisMap,
+};
 use hdlcparse::{type3::HdlcFrame, Error as HdlcError};
 use mbusparse::{Error as MBusError, Telegram};
 
@@ -43,9 +48,8 @@ where
         Ok(apdu) => apdu,
         Err(err) => return Some(Err(err)),
       };
-      match ObisMap::parse(&apdu) {
-        Ok((_, obis)) => return Some(Ok(obis)),
-        Err(_) => (),
+      if let Ok((_, obis)) = ObisMap::parse(&apdu) {
+        return Some(Ok(obis));
       }
     }
     None
@@ -59,43 +63,115 @@ impl<I> From<I> for ObisIterator<I> {
 }
 
 #[derive(Debug)]
-pub struct MBusSmartMeter<R> {
+pub struct SmartMeter<R, F> {
   reader: R,
-  data_link_layer: MBusSmartMeterDataLinkLayer,
+  dlms: Dlms,
   buffer: Vec<u8>,
+  _marker: PhantomData<F>,
 }
 
-impl<R> MBusSmartMeter<R> {
-  pub fn new(reader: R, dlms: Dlms<MBusDataLinkLayer>) -> ObisIterator<Self> {
-    Self {
-      reader,
-      data_link_layer: MBusSmartMeterDataLinkLayer { dlms },
-      buffer: Vec::new(),
-    }
-    .into()
+impl<R, F> SmartMeter<R, F> {
+  pub fn obis_iter(reader: R, dlms: Dlms) -> ObisIterator<Self> {
+    Self::apdu_iter(reader, dlms).into()
   }
 
-  pub fn new_apdu(reader: R, dlms: Dlms<MBusDataLinkLayer>) -> Self {
+  pub fn apdu_iter(reader: R, dlms: Dlms) -> Self {
     Self {
       reader,
-      data_link_layer: MBusSmartMeterDataLinkLayer { dlms },
+      dlms,
       buffer: Vec::new(),
+      _marker: PhantomData,
     }
   }
+
   pub fn reader(&mut self) -> &mut R {
     &mut self.reader
   }
 }
 
-impl<R> Iterator for MBusSmartMeter<R>
+impl<R, Dll> Iterator for SmartMeter<R, Dll>
 where
   R: Read,
+  for<'i> Dll: SmartMeterDataLinkLayer<'i>,
 {
   type Item = Result<Apdu, Error>;
 
   /// Get the next reading.
   fn next(&mut self) -> Option<Self::Item> {
-    parse_next(&mut self.buffer, &mut self.reader, &self.data_link_layer)
+    let mut bytes_needed = 0;
+    let mut telegrams_needed = 1;
+
+    'outer: loop {
+      match (&mut self.reader)
+        .take(bytes_needed as u64)
+        .read_to_end(&mut self.buffer)
+      {
+        Ok(_) => (),
+        Err(err) => return Some(Err(Error::Io(err))),
+      }
+
+      let mut telegrams = Vec::new();
+
+      let mut buffer = self.buffer.as_slice();
+      let mut telegram_1_len = 0;
+      let mut telegrams_len = 0;
+
+      for i in 0..telegrams_needed {
+        let err = match Dll::parse_frame(buffer).map_err(|err| err.into()) {
+          Ok((next_buffer, telegram)) => {
+            let telegram_len = buffer.len() - next_buffer.len();
+            if i == 0 {
+              telegram_1_len = telegram_len;
+            }
+            telegrams_len += telegram_len;
+            buffer = next_buffer;
+            telegrams.push(telegram);
+            None
+          }
+          Err(err) => Some(err),
+        };
+        match err {
+          Some(ParseError::Incomplete(n)) => {
+            bytes_needed = n.map(|b| b.get()).unwrap_or(1);
+            continue 'outer;
+          }
+          Some(ParseError::InvalidStart) => {
+            drop(telegrams);
+            self.buffer.remove(0);
+            bytes_needed = 0;
+            continue 'outer;
+          }
+          Some(ParseError::Other) => {
+            // Input is invalid but not incomplete,
+            // so try advancing the buffer.
+            drop(telegrams);
+            self.buffer.remove(0);
+            bytes_needed = 0;
+            continue 'outer;
+          }
+          None => (),
+        }
+      }
+      bytes_needed = 0;
+      match Dll::decrypt(&self.dlms, &telegrams[..]) {
+        Ok(apdu) => {
+          drop(telegrams);
+          self.buffer.drain(0..telegrams_len);
+          return Some(Ok(apdu));
+        }
+        Err(DlmsError::Incomplete(n)) => {
+          telegrams_needed += n.map(|t| t.get()).unwrap_or(1);
+        }
+        Err(DlmsError::InvalidFormat | DlmsError::ChecksumMismatch) => {
+          // Other error, continue with next telegram.
+          drop(telegrams);
+          self.buffer.drain(0..telegram_1_len);
+          telegrams_needed = 1;
+          continue;
+        }
+        Err(DlmsError::DecryptionFailed) => return Some(Err(Error::DecryptionFailed)),
+      }
+    }
   }
 }
 
@@ -128,166 +204,40 @@ impl From<HdlcError> for ParseError {
   }
 }
 
-pub trait SmartMeterDataLinkLayer<'i> {
-  type Frame: 'i;
+// unfortunatly rust does not have higher rank trait bounds for types
+pub trait SmartMeterDataLinkLayer<'i>: ParseFrame<'i> {
+  fn decrypt(dlms: &Dlms, frames: &[<Self as ParseFrame<'i>>::Frame]) -> Result<Apdu, DlmsError>;
+}
+
+impl<'i, T> SmartMeterDataLinkLayer<'i> for T
+where
+  for<'f> Self: DlmsDataLinkLayer<'i, &'f [<Self as ParseFrame<'i>>::Frame]> + ParseFrame<'i>,
+{
+  fn decrypt(dlms: &Dlms, frames: &[<Self as ParseFrame<'i>>::Frame]) -> Result<Apdu, DlmsError> {
+    dlms.decrypt_apdu::<T, _>(frames).map(|(_, apdu)| apdu)
+  }
+}
+
+pub trait ParseFrame<'i> {
+  type Frame;
   type Error: Into<ParseError>;
   fn parse_frame(input: &'i [u8]) -> Result<(&'i [u8], Self::Frame), Self::Error>;
-  fn decrypt(&self, frames: &[Self::Frame]) -> Result<Apdu, DlmsError>;
 }
 
-#[derive(Debug)]
-struct MBusSmartMeterDataLinkLayer {
-  dlms: Dlms<MBusDataLinkLayer>,
-}
-
-impl<'i> SmartMeterDataLinkLayer<'i> for MBusSmartMeterDataLinkLayer {
+impl<'i> ParseFrame<'i> for MBusDataLinkLayer {
   type Frame = Telegram<'i>;
   type Error = MBusError;
 
   fn parse_frame(input: &'i [u8]) -> Result<(&'i [u8], Self::Frame), Self::Error> {
     Telegram::parse(input)
   }
-
-  fn decrypt(&self, frames: &[Self::Frame]) -> Result<Apdu, DlmsError> {
-    self.dlms.decrypt_apdu(frames).map(|(_, apdu)| apdu)
-  }
 }
 
-#[derive(Debug)]
-struct HdlcSmartMeterDataLinkLayer {
-  dlms: Dlms<HdlcDataLinkLayer>,
-}
-
-impl<'i> SmartMeterDataLinkLayer<'i> for HdlcSmartMeterDataLinkLayer {
+impl<'i> ParseFrame<'i> for HdlcDataLinkLayer {
   type Frame = HdlcFrame<'i>;
   type Error = HdlcError;
-  fn parse_frame(input: &'i [u8]) -> Result<(&'i [u8], HdlcFrame<'i>), HdlcError> {
+
+  fn parse_frame(input: &'i [u8]) -> Result<(&'i [u8], Self::Frame), Self::Error> {
     HdlcFrame::parse(input)
-  }
-  fn decrypt(&self, frames: &[Self::Frame]) -> Result<Apdu, DlmsError> {
-    self.dlms.decrypt_apdu(frames).map(|(_, apdu)| apdu)
-  }
-}
-
-fn parse_next<R, Smdll>(
-  buffer: &mut Vec<u8>,
-  reader: &mut R,
-  parse_frame: &Smdll,
-) -> Option<Result<Apdu, Error>>
-where
-  R: Read,
-  Smdll: for<'i> SmartMeterDataLinkLayer<'i>,
-{
-  let mut bytes_needed = 0;
-  let mut telegrams_needed = 1;
-
-  'outer: loop {
-    match (reader).take(bytes_needed as u64).read_to_end(buffer) {
-      Ok(_) => (),
-      Err(err) => return Some(Err(Error::Io(err))),
-    }
-
-    let mut telegrams = Vec::new();
-
-    let mut buf = buffer.as_slice();
-    let mut telegram_1_len = 0;
-    let mut telegrams_len = 0;
-
-    for i in 0..telegrams_needed {
-      let err = match Smdll::parse_frame(buf).map_err(|err| err.into()) {
-        Ok((next_buffer, telegram)) => {
-          let telegram_len = buf.len() - next_buffer.len();
-          if i == 0 {
-            telegram_1_len = telegram_len;
-          }
-          telegrams_len += telegram_len;
-          buf = next_buffer;
-          telegrams.push(telegram);
-          None
-        }
-        Err(err) => Some(err),
-      };
-      match err {
-        Some(ParseError::Incomplete(n)) => {
-          bytes_needed = n.map(|b| b.get()).unwrap_or(1);
-          continue 'outer;
-        }
-        Some(ParseError::InvalidStart) => {
-          drop(telegrams);
-          buffer.remove(0);
-          bytes_needed = 0;
-          continue 'outer;
-        }
-        Some(ParseError::Other) => {
-          // Input is invalid but not incomplete,
-          // so try advancing the buffer.
-          drop(telegrams);
-          buffer.remove(0);
-          bytes_needed = 0;
-          continue 'outer;
-        }
-        None => (),
-      }
-    }
-    bytes_needed = 0;
-
-    match parse_frame.decrypt(&telegrams) {
-      Ok(apdu) => {
-        drop(telegrams);
-        buffer.drain(0..telegrams_len);
-        return Some(Ok(apdu));
-      }
-      Err(DlmsError::Incomplete(n)) => {
-        telegrams_needed += n.map(|t| t.get()).unwrap_or(1);
-      }
-      Err(DlmsError::InvalidFormat | DlmsError::ChecksumMismatch) => {
-        // Other error, continue with next telegram.
-        drop(telegrams);
-        buffer.drain(0..telegram_1_len);
-        telegrams_needed = 1;
-        continue;
-      }
-      Err(DlmsError::DecryptionFailed) => return Some(Err(Error::DecryptionFailed)),
-    }
-  }
-}
-
-#[derive(Debug)]
-pub struct HdlcSmartMeter<R> {
-  reader: R,
-  data_link_layer: HdlcSmartMeterDataLinkLayer,
-  buffer: Vec<u8>,
-}
-
-impl<R> HdlcSmartMeter<R> {
-  pub fn new(reader: R, dlms: Dlms<HdlcDataLinkLayer>) -> ObisIterator<Self> {
-    Self {
-      reader,
-      data_link_layer: HdlcSmartMeterDataLinkLayer { dlms },
-      buffer: Vec::new(),
-    }
-    .into()
-  }
-
-  pub fn apdu_iter(reader: R, dlms: Dlms<HdlcDataLinkLayer>) -> Self {
-    Self {
-      reader,
-      data_link_layer: HdlcSmartMeterDataLinkLayer { dlms },
-      buffer: Vec::new(),
-    }
-  }
-  pub fn reader(&mut self) -> &mut R {
-    &mut self.reader
-  }
-}
-
-impl<R> Iterator for HdlcSmartMeter<R>
-where
-  R: Read,
-{
-  type Item = Result<Apdu, Error>;
-
-  fn next(&mut self) -> Option<Self::Item> {
-    parse_next(&mut self.buffer, &mut self.reader, &self.data_link_layer)
   }
 }
